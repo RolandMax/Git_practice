5ddfd8980afdcf75a338ed8b7b6f85a627aa5d6c
RolandMax <rolandmaximillianholzinger@gmail.com>
2024-10-04 14:47:10 +0200

    added crazy function

--- scripts/new_R_script.R
+++ scripts/new_R_script.R
@@ -9,3 +9,18 @@
 10
 
 # keep new line
+
+# function
+crazy_number <- function(x){
+  y <- x + 3 
+  return(y)}
+
+crazy_number(3)
+
+# function w/ function 
+crazy_function <- function(x){
+  y <- crazy_number(x) + 3
+  return(y)
+}
+
+crazy_function(4)
